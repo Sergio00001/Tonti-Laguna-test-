f7da2ca38fcb1d4bc30a7660334164410fd66728
Sergio00001 <digtyarsergio@gmail.com>
2022-09-10 17:56:23 +0300

    deploy branch

--- src/js/files/script.js
+++
@@ -1,7 +0,0 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
-// Подключение модуля по управлению тарифами
-import { taxToggle } from "./taxToggle.js";
-taxToggle()
